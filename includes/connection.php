509f800fb05b575723f9705b8185bad0c9e3278b
chaudhariamanullah <chaudhari.amanullah0@gmail.com>
2025-08-12 12:13:45 +0530

    Attempt 1

--- includes/connection.php
+++ includes/connection.php
@@ -1,13 +1,15 @@
 <?php
 
-    $host = "localhost";
-    $user = "root";
-    $pass = "";
-    $db   = "todo";
+   $host = "mysql-2c38a4ee-chaudhari-c6c0.e.aivencloud.com";
+    $port = 24942;
+    $user = "avnadmin";
+    $pass = "YOUR_PASSWORD";
+    $db   = "defaultdb";
 
-    $conn = new mysqli($host, $user, $pass, $db);
+    $conn = new mysqli($host, $user, $pass, $db, $port);
 
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
+
 ?>
